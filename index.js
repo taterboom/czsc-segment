048649cf85e0f23bc30c9bbaa9c76605cdcea6da
xueyong <1085523029@qq.com>
2022-12-09 17:43:26 +0800

    ✨ add toc

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const mammoth = require("mammoth")
 const fs = require("node:fs/promises")
 const path = require("node:path")
+const mammoth = require("mammoth")
+const markdownToc = require("markdown-toc")
 
 mammoth
   //   .convertToHtml({ path: path.join(__dirname, "../缠中说禅全部博客内容.docx") })
@@ -10,31 +11,41 @@ mammoth
   //   })
   .extractRawText({ path: path.join(__dirname, "./缠中说禅全部博客内容.docx") })
   .then((res) => {
-    fs.writeFile(
-      "output.md",
-      res.value
-        .split(/(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}.*\n\n)/)
-        .slice(1)
-        .reduce((total, chunk, index) => {
-          const totalIndex = Math.floor(index / 2)
-          if (index % 2 === 0) {
-            if (chunk.includes("教你炒股票")) {
-              total[totalIndex] = `## ${chunk.trim()}`
-            } else {
-              total[totalIndex] = ""
-            }
-          } else {
-            if (chunk.trim() && total[totalIndex]) {
-              total[totalIndex] += `\n> ${chunk.replace("\n\n", "\n").replace("html", "html\n")}`
-            } else {
-              total[totalIndex] = ""
-            }
-          }
-          return total
-        }, [])
-        .filter(Boolean)
-        .join("\n"),
-      "utf-8"
-    )
+    const title = "缠中说缠-教你炒股票系列.md"
+    fs.access(title)
+      .then((res) => {
+        console.error(`"${title}" exists, remove it before generating.`)
+      })
+      .catch(() => {
+        fs.writeFile("缠中说缠-教你炒股票系列.md", generateMarkdown(res.value), "utf-8")
+      })
   })
-  .done()
+
+function generateMarkdown(raw) {
+  let mdText = raw
+    .split(/(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}.*\n\n)/)
+    .slice(1)
+    .reduce((total, chunk, index) => {
+      const totalIndex = Math.floor(index / 2)
+      if (index % 2 === 0) {
+        if (chunk.includes("教你炒股票")) {
+          total[totalIndex] = `## ${chunk.trim()}`
+        } else {
+          total[totalIndex] = ""
+        }
+      } else {
+        if (chunk.trim() && total[totalIndex]) {
+          total[totalIndex] += `\n> ${chunk.replace("\n\n", "\n").replace("html", "html\n")}`
+        } else {
+          total[totalIndex] = ""
+        }
+      }
+      return total
+    }, [])
+    .filter(Boolean)
+    .join("\n")
+  mdText = `<!-- toc -->\n` + mdText
+  mdText = markdownToc.insert(mdText)
+
+  return mdText
+}
